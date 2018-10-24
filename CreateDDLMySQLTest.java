5a5e0619869cfb283d7fcbabd0303d1f5345f857
Brajon Andrews <bla3695@g.rit.edu>
2018-10-24 13:50:09 -0400

    Add jar files and started test file for CreateDDLMySQLTest
    Added the jar files to run the test using junit. Removed the copyright symbol in the EdgeConvertGUI because it was causing an error. Started writing the test class for CreateDDLMySQLTest.

---
+++ CreateDDLMySQLTest.java
@@ -0,0 +1,11 @@
+//import staticorg.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CreateDDLMySQLTest 
+{
+	public void evaluatesExpression() 
+	{
+		CreateDDLMySQL test1 = new CreateDDLMySQL();
+		test1.createDDL();
+	}
+}
