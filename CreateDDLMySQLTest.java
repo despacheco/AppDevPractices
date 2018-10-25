1123e24a0e33cc1d5481785773eec39ac848275e
Brajon Andrews <bla3695@g.rit.edu>
2018-10-25 16:46:47 -0400

    Update CreateDDLMySQLTest.java

--- CreateDDLMySQLTest.java
+++ CreateDDLMySQLTest.java
@@ -1,11 +1,19 @@
-//import staticorg.junit.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class CreateDDLMySQLTest 
 {
+	@Test
 	public void evaluatesExpression() 
 	{
 		CreateDDLMySQL test1 = new CreateDDLMySQL();
-		test1.createDDL();
+		int message = test1.convertStrBooleanToInt("true");
+		
+		assertEquals(0,message);
 	}
 }
+
+// Run test in terminal
+// javac -cp .:junit-4.12.jar CreateDDLMySQLTest.java
+// javac CreateDDLMySQL.java
+// java -cp .:junit-4.12.jar:hamcrest-core-1.3.jar \org.junit.runner.JUnitCore CreateDDLMySQLTest
