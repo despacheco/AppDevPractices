d5aaaeee88ff081f317438aba395feb124ad9631
Brajon Andrews <bla3695@g.rit.edu>
2018-10-26 13:00:12 -0400

    EvaluateCreatedDDL
    Add a test for evaluateCreatedDDL.

--- CreateDDLMySQLTest.java
+++ CreateDDLMySQLTest.java
@@ -1,16 +1,42 @@
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
 
 public class CreateDDLMySQLTest 
 {
+   private CreateDDLMySQL test1 = new CreateDDLMySQL();
+   
+   @Before
+   public void init()
+   {
+      String[] strDataType = {"VARCHAR", "BOOL", "INT", "DOUBLE"};
+   }
+   
+   @Test
+   public void evaluateCreatedDDL()
+   {
+      StringBuffer sb = new StringBuffer();
+      test1.createDDL();
+      sb.append("hello");
+      assertEquals("Create DDL:",sb,test1.getSQLString());
+   }
 	@Test
 	public void evaluatesExpression() 
 	{
-		CreateDDLMySQL test1 = new CreateDDLMySQL();
-		int message = test1.convertStrBooleanToInt("true");
-		
-		assertEquals(0,message);
+		int actual = test1.convertStrBooleanToInt("true");
+		int expected = 1;
+		assertEquals(expected, actual);
 	}
+   
+   // @Test
+//    public void evaluateGenerateDatabaseName()
+//    {
+//      String actual  = test1.generateDatabaseName();
+//      String expected = "MySQLDB";
+//      assertEquals("Name of the database:","hello",databaseName);
+//    }
 }
 
 // Run test in terminal
