541b1a05f46bf5d016871a3e581d2ef1ec4a513a
Brajon Andrews <bla3695@g.rit.edu>
2018-10-26 20:32:16 -0400

    Latest
    Still not working

--- CreateDDLMySQLTest.java
+++ CreateDDLMySQLTest.java
@@ -5,13 +5,19 @@ import org.junit.After;
 
 
 public class CreateDDLMySQLTest 
-{
-   private CreateDDLMySQL test1 = new CreateDDLMySQL();
-   
+{  
+   //EdgeTable[] inputTables = new EdgeTable[5];
+   //inputTables = new EdgeTable("Students|Courses");
+//    inputTables.add("Student|Courses");  
+//    EdgeField[] inputFields = new EdgeField[1];
+   //CreateDDLMySQL test1 = new CreateDDLMySQL(inputTables,inputFields);
+   CreateDDLMySQL test1 = new CreateDDLMySQL();
    @Before
    public void init()
    {
-      String[] strDataType = {"VARCHAR", "BOOL", "INT", "DOUBLE"};
+      //String[] strDataType = {"VARCHAR", "BOOL", "INT", "DOUBLE"};
+      //StringBuffer sb = new StringBuffer();
+      //sb.append("New");
    }
    
    @Test
@@ -19,16 +25,18 @@ public class CreateDDLMySQLTest
    {
       StringBuffer sb = new StringBuffer();
       test1.createDDL();
-      sb.append("hello");
       assertEquals("Create DDL:",sb,test1.getSQLString());
    }
-	@Test
-	public void evaluatesExpression() 
-	{
-		int actual = test1.convertStrBooleanToInt("true");
-		int expected = 1;
-		assertEquals(expected, actual);
-	}
+	// @Test
+// 	public void evaluatesExpression() 
+// 	{
+//       CreateDDLMySQL test1 = new CreateDDLMySQL();
+// 		int actual = test1.convertStrBooleanToInt("true");
+// 		int expected = 1;
+// 		assertEquals(expected, actual);
+// 	}
+   
+   
    
    // @Test
 //    public void evaluateGenerateDatabaseName()
