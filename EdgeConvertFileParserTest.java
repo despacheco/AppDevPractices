397a9b363d1e870714a75c796891fa7f74c6a67d
Des <despacheco06@gmail.com>
2018-10-29 23:16:12 -0400

    Test class

--- EdgeConvertFileParserTest.java
+++ EdgeConvertFileParserTest.java
@@ -1,17 +1,36 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
+import org.junit.rules.TemporaryFolder;
 import org.junit.Test;
+import org.junit.Rule;
 import java.io.*;
+import java.net.URL;
 
-public class EdgeConvertFileParserTest 
+public class EdgeConvertFileParserTest
 {
-	File courses = new File("Courses.edg"); 
-	@Test
-	public void evaluatesFile() 
-	{
-		EdgeConvertFileParser test1 = new EdgeConvertFileParser(courses);
-		//File edgeFile = test1.openFile(courses);
-		//assertEquals("Courses.edg",edgeFile);
-	}
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
+    public final File coursesFile = new File("Courses.edg"); 
+
+    @Test
+    public void testFileClassLoader() {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        File courseFile = new File(classLoader.getResource("Courses.edg").getFile());
+        assertTrue(courseFile.exists());
+    }
+
+    public void testReadFileWithResource() {
+        URL url = this.getClass().getResource("Courses.edg");
+        File coursesFile = new File(url.getFile());
+        assertTrue(coursesFile.exists());
+    }
+
+    public void testReadFileWithResource2() throws IOException {
+        InputStream inStream = this.getClass().getResourceAsStream("Courses.edg");
+        assertNotNull(inStream);
+    }
+
 }
 
 // Run test in terminal
